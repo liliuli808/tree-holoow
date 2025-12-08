40df11f65d531cb9290ab156cb63c54139b90f1b
jichi <liliuli808@gmail.com>
2025-12-08 20:41:52 +0800

    chore: 添加 Capacitor 移动端配置及相关依赖
    添加 Capacitor 配置文件及移动端平台目录到 gitignore
    更新 package.json 添加 Capacitor 相关依赖

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: '树洞交友-(tree-hollow)',
+  webDir: 'dist'
+};
+
+export default config;
